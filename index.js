5e0aa0d2860e68b2b7d568df91e67dfde75be641
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2022-08-06 20:31:08 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendCat(name){
+  destructivelyAppendCat = cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name){
+  destructivelyPrependCat = cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(name){
+  destructivelyRemoveLastCat = cats.pop()
+}
+
+function destructivelyRemoveFirstCat(name){
+  destructivelyRemoveFirstCat = cats.shift()
+}
+
+function appendCat(name){
+  return [...cats, "Broom"];
+}
+
+function prependCat(name){
+  return ["Arnold", ...cats];
+}
+console.log(prependCat)
+
+function removeLastCat(){
+  return cats.slice(0,2)
+}
+
+function removeFirstCat(){
+  return cats.slice(1)
+}
